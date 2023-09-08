eb23b10b36f827ebb22c1d1a10d5d047c8757d4b
lewiskimaru <139235175+lewiskimaru@users.noreply.github.com>
2023-09-08 18:02:11 +0300

    Update rps.py

--- rps.py
+++ rps.py
@@ -41,4 +41,5 @@ webrtc_ctx = webrtc_streamer(
     rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
     video_processor_factory=VideoProcessor,
-    async_processing=True,
+    async_processing=True
+)
